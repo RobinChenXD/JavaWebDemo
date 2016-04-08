0c9edd37460d1d5603ad8f190ffd64e51baf4321
chenyuze <748035300@qq.com>
2016-04-08 22:07:04 +0800

    Added files via upload

--- Question.java
+++ Question.java
@@ -1,54 +1,38 @@
-package Question1_3;
+package Question1_4;
 
-public class Question {	
-	public static String sort(String s) {
-		char[] content = s.toCharArray();
-		java.util.Arrays.sort(content);
-		return new String(content);
-	}
-	
-	public static boolean permutation(String s, String t) {
-		return sort(s).equals(sort(t));
-	}
-	
-	public static boolean anagram(String s, String t) {
-		if (s.length() != t.length())
-			return false;
-		int[] letters = new int[128];
-		int num_unique_chars = 0;
-		int num_completed_t = 0;
-		char[] s_array = s.toCharArray();
-		for (char c : s_array) { // count number of each char in s.
-			if (letters[c] == 0)
-				++num_unique_chars;
-			++letters[c];
-		}
-		for (int i = 0; i < t.length(); ++i) {
-			int c = (int) t.charAt(i);
-			if (letters[c] == 0) { // Found more of char c in t than in s.
-				return false;
-			}
-			--letters[c];
-			if (letters[c] == 0) {
-				++num_completed_t;
-				if (num_completed_t == num_unique_chars) {
-					// itÕs a match if t has been processed completely
-					return true;
-					//return i == t.length() - 1;
-				}
+import CtCILibrary.AssortedMethods;
+
+public class Question {
+	// Assume string has sufficient free space at the end
+	public static void replaceSpaces(char[] str, int length) {
+		int spaceCount = 0, index, i = 0;
+		for (i = 0; i < length; i++) {
+			if (str[i] == ' ') {
+				spaceCount++;
 			}
 		}
-		return false;
-	}	
+		index = length + spaceCount * 2;
+		str[index] = '\0';
+		for (i = length - 1; i >= 0; i--) {
+			if (str[i] == ' ') {
+				str[index - 1] = '0';
+				str[index - 2] = '2';
+				str[index - 3] = '%';
+				index = index - 3;
+			} else {
+				str[index - 1] = str[i];
+				index--;
+			}
+		}
+	}
 	
 	public static void main(String[] args) {
-		String[][] pairs = {{"apple", "papel"}, {"carrot", "tarroc"}, {"hello", "llloh"}};
-		for (String[] pair : pairs) {
-			String word1 = pair[0];
-			String word2 = pair[1];
-			boolean anagram = permutation(word1, word2);
-			System.out.println(word1 + ", " + word2 + ": " + anagram);
-			System.out.println(anagram(word1, word2));
+		String str = "abc d e f";
+		char[] arr = new char[str.length() + 3 * 2 + 1];
+		for (int i = 0; i < str.length(); i++) {
+			arr[i] = str.charAt(i);
 		}
+		replaceSpaces(arr, str.length());	
+		System.out.println("\"" + AssortedMethods.charArrayToString(arr) + "\"");
 	}
 }
